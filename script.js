5033330e730be2133310ae1937ebd74570c0dfce
Xinggggggggggg <159937658+Xinggggggggggg@users.noreply.github.com>
2024-02-14 00:17:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const valentines = document.querySelector(".valentines");
+const envelop = document.querySelector(".envelope");
+const front = document.querySelector(".front");
+const card = document.querySelector(".card");
+const text = document.querySelector(".text");
+const heart = document.querySelector(".heart");
+const hearts = document.querySelector(".heart");
+
+heart.addEventListener("click", () => {
+    envelop.innerHTML = "Here a flower for youuu";
+    heart.src = "https://github.com/JessicaaSun/flower-blooming/blob/main/style.css";
+});
